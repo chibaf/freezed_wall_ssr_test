217c253ae387d65fc950d801819a6a4ba549a0f9
chibaf <chibaf@mac.com>
2024-02-25 04:59:56 +0900

    Add files via upload

--- ssr_heat.py
+++ ssr_heat.py
@@ -2,13 +2,13 @@ import RPi.GPIO as GPIO
 from time import sleep
 import time
 
-ssr_pin = 12
+ssr_pin = 5
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(ssr_pin,GPIO.OUT)
 
 while True:
-  pwm_totaltime=1.0
+  pwm_totaltime=10.0
   pwm_width=0.5
   on_time=pwm_totaltime*pwm_width
   off_time=pwm_totaltime*(1-pwm_width)
